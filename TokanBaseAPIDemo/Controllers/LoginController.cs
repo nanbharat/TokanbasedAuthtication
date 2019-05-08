a9473501845d02eefaaca5148bad379c3b39e7b8
nanbharat <bharat.199120@gmail.com>
2019-05-08 15:53:26 +0530

    .

---
+++ TokanBaseAPIDemo/Controllers/LoginController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using TokanBaseAPIDemo.Model;
+
+namespace TokanBaseAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody]UserModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+        private UserModel AuthenticateUser(UserModel user)
+        {
+            UserModel user1 = null;
+
+            //Validate the User Credentials  
+            //Demo Purpose, I have Passed HardCoded User Information  
+            if (user.Username == "Jignesh")
+            {
+                user1 = new UserModel { Username = "Jignesh Trivedi", EmailAddress = "test.btest@gmail.com" };
+            }
+
+            return ( null == user1 ) ? null: user1;
+
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
